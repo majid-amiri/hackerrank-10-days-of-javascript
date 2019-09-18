7180883a266a4dcead1aff87e878587c7831a65f
Majid Amiri <45729098+majid-amiri@users.noreply.github.com>
2019-09-18 13:09:37 +0430

    Create day3-arrays.js

---
+++ day3-arrays.js
@@ -0,0 +1,38 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/**
+*   Return the second largest number in the array.
+*   @param {Number[]} nums - An array of numbers.
+*   @return {Number} The second largest number in the array.
+**/
+function getSecondLargest(nums) {
+    // Complete the function
+    const temp = [];
+    nums.sort((a, b) => b - a).forEach(n => {
+        if(!temp.includes(n))
+            temp.push(n)
+    })
+    return temp[1];
+}
