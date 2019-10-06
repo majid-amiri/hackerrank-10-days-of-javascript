af2e5f2e7151489e717308bfeab6cece0a74e4b3
Majid Amiri <45729098+majid-amiri@users.noreply.github.com>
2019-10-06 15:08:17 +0330

    Create day4-count-objects.js

---
+++ day4-count-objects.js
@@ -0,0 +1,33 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Return a count of the total number of objects 'o' satisfying o.x == o.y.
+ * 
+ * Parameter(s):
+ * objects: an array of objects with integer properties 'x' and 'y'
+ */
+function getCount(objects) {
+    return objects.filter(o => o.x === o.y).length
+}
